21f262bfd1899188145dc492e90e995c22cacbe9
XiaoFeiDu <romanchaos@163.com>
2021-05-10 16:58:50 +0800

    Create demo projects

---
+++ demo/Pair/PairA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PairA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
