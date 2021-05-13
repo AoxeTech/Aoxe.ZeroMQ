4edf56a942492e1dd2a652520f6675c5bd1dc69a
XiaoFeiDu <romanchaos@163.com>
2021-05-13 11:24:17 +0800

    Complete client-server demo.

--- demo/RequestReply/ClientServer/ClientServer/Program.cs
+++ demo/RequestReply/ClientServer/ClientServer/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace ClientServer
+﻿using System;
+using System.Text;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace ClientServer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using var server = new ServerSocket();
+            using var client = new ClientSocket();
+            server.Bind("inproc://client-server");
+            client.Connect("inproc://client-server");
+
+            client.Send(Encoding.UTF8.GetBytes("Hello"));
+            Console.WriteLine("Client has sent \"Hello\" to server.");
+            
+            var (routingId, clientMsg) = server.ReceiveBytes();
+            
+            Console.WriteLine($"Server received \"{Encoding.UTF8.GetString(clientMsg)}\" from client which routing Id [{routingId}]");
+
+            server.Send(routingId, "World");
+            Console.WriteLine($"Server sent \"World\" to routing Id [{routingId}]");
+            var serverMsg = client.ReceiveString();
+            Console.WriteLine($"Client received \"{serverMsg}\" from server.");
+            Console.ReadLine();
         }
     }
 }
