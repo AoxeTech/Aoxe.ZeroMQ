69c6ee18f13d45ad9f56b76fb60181c1eb9538b2
Mutudu <aeondxf@live.com>
2024-06-20 11:28:42 +0800

    Remove unused serializers.

--- tests/Aoxe.ZeroMQ.Test/GlobalUsings.cs
+++ tests/Aoxe.ZeroMQ.Test/GlobalUsings.cs
@@ -1,6 +1,5 @@
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Threading.Tasks;
-global using Aoxe.SystemTextJson;
 global using TestModels;
 global using Xunit;
--- tests/TestModels/GlobalUsings.cs
+++ tests/TestModels/GlobalUsings.cs
@@ -1,5 +1,3 @@
 // Global using directives
 
 global using System;
-global using ProtoBuf;
-global using ZeroFormatter;
